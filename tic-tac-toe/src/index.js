2e92dc47f3ec0bc59bbdcceca0046f12a4da3dc6
smita patil <smitakpatil26@gmail.com>
2019-11-05 05:07:52 +0530

    square class to function

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -10,25 +10,56 @@ serviceWorker.unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-class Square extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={value:null,}
-    }
-    render(){
-        return(
-            <button className="Square" onClick={()=>this.setState({value:"X"})}>
-                {/* {this.props.value} */}
-                {this.state.value}
-            </button>
-        );
-    }
+// class Square extends React.Component{
+//     // constructor(props){
+//     //     super(props);
+//     //     this.state={value:null,}
+//     // }
+//     render(){
+//         return(
+//             // <button className="Square" onClick={()=>this.setState({value:"X"})}>
+//             <button className="square" onClick={()=>this.props.onClick()}>
+//                 {this.props.value}
+//             </button>
+//                 // {* {this.state.value} */}
+            
+//         );
+//     }
+// }
+
+//square class changed to function
+function Square(props){
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
  
-class Board extends React.Component{
-    renderSquare(i){
-        return <Square value={i}/>;
+class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        
+      };
     }
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+  
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+  
     render(){
         const status='Next player: X';
         return(
